05b5a8d9650955da9932644fbd2b8d8a138d9686
José Lucas <124110365+jlfaguiar@users.noreply.github.com>
2024-05-13 21:36:59 -0300

    fix: aprimorada responsividade da tela de login

--- Front-End/facens_caronas/Screens/Login/Login.jsx
+++ Front-End/facens_caronas/Screens/Login/Login.jsx
@@ -6,7 +6,6 @@ import { auth } from "../../Services/firebaseConfig";
 import { setPersistence } from "firebase/auth";
 
 
-
 const loginStyles = StyleSheet.create({
   login: {
     backgroundColor: '#ffffff',
@@ -30,11 +29,10 @@ const loginStyles = StyleSheet.create({
     width: 360,
   },
   'overlap-group': {
-    height: 46,
-    left: 30,
-    position: 'absolute',
-    top: 267,
-    width: 300,
+    height: '5%',
+    width: '85%',
+    flex: 1,
+    alignSelf: 'center'
   },
   'rectangle': {
     backgroundColor: '#d9d9d9',
@@ -65,54 +63,34 @@ const loginStyles = StyleSheet.create({
     width: 300,
     borderRadius: 20,
     borderWidth: 5,
-    borderColor: '#1e22e8'
+    borderColor: '#1e22e8',
+    flex: 1
   },
   'button': {
     backgroundColor: '#5CC6BA',
     borderRadius: 80,
-    top: 325,
-    width: 300,
-    position: 'absolute',
+    position: 'relative',
     alignContent: 'center',
     alignItems: 'center',
-    height: 46,
-    left: 30,
+    alignSelf: 'center',
     justifyContent: 'center',
     alignItems: 'center',
-    alignContent: 'center'
-  },
-
-  'overlap-2': {
-    height: 46,
-    left: 30,
-    position: 'absolute',
-    top: 335,
-    width: 300,
+    alignContent: 'center',
+    height: '1%',
+    width: '85%',
+    maxHeight: '7%',
+    flex: 1,
+    marginBottom: 50,
+    marginTop: 50,
   },
   'text-cad': {
     color: '#a0a0a0',
     fontFamily: 'Roboto',
     fontSize: 14,
-    position: 'absolute',
+    position: 'relative',
     textAlign: 'center',
-    top: '85%',
-    alignSelf: 'center'
-  },
-  'text-cad-passageiro': {
-    color: '#a0a0a0',
-    fontFamily: 'Roboto',
-    fontSize: 14,
-    position: 'absolute',
-    top: '90%',
-    alignSelf: 'center'
-  },
-  'text-cad-motorista': {
-    color: '#a0a0a0',
-    fontFamily: 'Roboto',
-    fontSize: 14,
-    position: 'absolute',
-    top: '95%',
-    alignSelf: 'center'
+    flex: 2,
+    alignSelf: 'center',
   },
   'container': {
     flex: 1,
@@ -157,14 +135,9 @@ export const Login = (props) => {
   return (
     <View style={loginStyles.container}>
       <View style={loginStyles.View}>
-
-        {/* <Text style={{
-          color: '#5CC6BA',
-          alignSelf: 'center',
-          fontSize: 30,
-        }}>FACENS Caronas</Text> */}
-
-        <Image  style={{alignSelf: 'center'}} source={require('./imgs/Logo.png')} />
+        <View style={{flex: 3, overflow: 'hidden'}}>
+          <Image  style={{alignSelf: 'center',}} source={require('./imgs/Logo.png')} />
+        </View>
 
         <View style={loginStyles["overlap-group"]}>
           <View style={loginStyles.rectangle}/>
@@ -173,7 +146,14 @@ export const Login = (props) => {
           }/>
         </View>
 
-        <View style={{flexDirection: 'row', flex: 1, alignContent: 'center'}}>
+        <View style={loginStyles["overlap-group"]}>
+          <View style={loginStyles.rectangle}/>
+          <TextInput style={loginStyles["text-wrapper"]} placeholder="Senha" onChangeText={
+            (field) => setSenha(field)
+          } secureTextEntry={true}/>
+        </View>
+
+
           <TouchableOpacity title='Entrar' onPress= {() => {
               loginStatus = userLogin();
               if (loginStatus) {
@@ -181,29 +161,21 @@ export const Login = (props) => {
             }} style={loginStyles["button"]}>
               <Text style={{fontSize: 20, textAlign:'auto', textAlignVertical: 'top', color: 'white'}}>Acessar</Text>
           </TouchableOpacity>
+
+
+        <View style={{flex: 1, flexDirection: 'column'}}>
+          <Text  style={loginStyles["text-cad"]}>
+            Não possuí conta? Cadastre-se!
+          </Text>
+
+          <Text onPress={() =>{props.navigation.navigate('screenRegister')}} style={loginStyles["text-cad"]}>
+            Cadastrar como Passageiro
+          </Text>
+
+          <Text onPress={() =>{props.navigation.navigate('screenRegisterMotorista')}} style={loginStyles["text-cad"]}>
+            Cadastrar como Motorista
+          </Text>
         </View>
-        
-        <View style={loginStyles["overlap-2"]}>
-          <View style={loginStyles.rectangle}/>
-          <TextInput style={loginStyles["text-wrapper"]} placeholder="Senha" onChangeText={
-            (field) => setSenha(field)
-          } secureTextEntry={true}/>
-
-
-        </View>
-        <Text  style={loginStyles["text-cad"]}>
-          Não possuí conta? Cadastre-se!
-        </Text>
-
-        <Text onPress={() =>{props.navigation.navigate('screenRegister')}}
-         style={loginStyles["text-cad-passageiro"]}>
-          Cadastrar como Passageiro
-        </Text>
-
-        <Text onPress={() =>{props.navigation.navigate('screenRegisterMotorista')}}
-         style={loginStyles["text-cad-motorista"]}>
-          Cadastrar como Motorista
-        </Text>
       </View>
     </View>
   );
